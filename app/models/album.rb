f5278fc455316c70710cb7d7c4bcd7988bac7ec1
Ali Jendoubi <alijandhd@gmail.com>
2022-12-12 22:00:23 +0100

    Add album model methods

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,24 @@ class Album < ApplicationRecord
   has_many :tracks, dependent: :delete_all
 
   include ImageUploader::Attachment(:image)
+
+  def self.search_albums(params)
+    if params[:query].blank?
+      []
+    else
+      where(
+        'lower(name) LIKE :query OR lower(description) LIKE :query', query: "%#{params[:query].downcase}%"
+      )
+    end
+  end
+
+  def self.search_artists(params)
+    if params[:query].blank?
+      []
+    else
+      where(
+        'lower(artist) LIKE :query', query: "%#{params[:query].downcase}%"
+      )
+    end
+  end
 end
