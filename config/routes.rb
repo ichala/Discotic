30ea5287aa115da5665c30ef9c6798760a3a4bf9
Ali Jendoubi <alijandhd@gmail.com>
2022-12-12 12:48:54 +0100

    Destroy track route & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  
+
   post 'track' => 'track#create', as: :tracks
+  delete 'track/:album_id/:track_id' => 'track#destroy', as: :track_destroy
   resources :albums do
   get '/myalbums/:page', action: :myalbums, on: :collection , as: :my
   get '/myalbums/', action: :myalbums, on: :collection 
