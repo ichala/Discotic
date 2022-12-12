4e16cd673cb3062d142b103f8ac352b9d3e27513
Ali Jendoubi <alijandhd@gmail.com>
2022-12-12 12:48:34 +0100

    Add destroy track path

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -3,8 +3,15 @@ class TrackController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @album = Album.find_by_id(params[:album_id])
+    @album = current_user.albums.find_by_id(params[:album_id])
     @track = @album.tracks.create(track_params)
+    redirect_to album_path(@album)
+  end
+
+  def destroy
+    @album = current_user.albums.find_by_id(delete_params[:album_id])
+    @track = @album.tracks.find_by_id(delete_params[:track_id])
+    @track.destroy
     redirect_to album_path(@album)
   end
 
@@ -13,4 +20,10 @@ class TrackController < ApplicationController
   def track_params
     params.require(:track).permit(:name, :time, :spotify_link, :album_id)
   end
+
+  def delete_params
+    params.permit(:album_id, :track_id)
+  end
+
+
 end
