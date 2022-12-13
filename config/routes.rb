b275d4bd2f60b7f5c223997104febf4fdadf60e7
Ali Jendoubi <alijandhd@gmail.com>
2022-12-13 15:44:55 +0100

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'explore' => 'explore#index' , as: :explore
   get 'search' => 'search#index' , as: :search
   post 'track' => 'track#create', as: :tracks
   delete 'track/:album_id/:track_id' => 'track#destroy', as: :track_destroy
