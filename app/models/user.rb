aa6cb91c44035c0da83c1869c972358eb468fd03
Ali Jendoubi <alijandhd@gmail.com>
2022-12-14 15:50:29 +0100

    Add Fav association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :albums, dependent: :delete_all
+  has_many :favorites, dependent: :delete_all
 end
